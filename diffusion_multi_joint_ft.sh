aa0e401e628b5fa94063f6bc99fd6e9b0106b136
taehyeon_eum <paul994782@gamil.com>
2024-01-22 09:51:41 +0000

    0122_ex12

--- diffusion_multi_joint_ft.sh
+++ diffusion_multi_joint_ft.sh
@@ -1,31 +1,31 @@
-MODEL_NAME="CompVis/stable-diffusion-v1-4"
-OUTPUT_DIR="./logs/chris_pratt_gog_background1_2"
-CONCEPT_LIST="./assets/concept_list_gog.json"
-DELTA_CKPT="logs/chris_pratt_gog_background1_2/delta.bin"
-FROM_FILE="prompts/gog_chris_pratt_background3.txt"
-KEYWORD="base_setting"
+# MODEL_NAME="CompVis/stable-diffusion-v1-4"
+# OUTPUT_DIR="./logs/chris_pratt_gog_background1_2"
+# CONCEPT_LIST="./assets/concept_list_gog.json"
+# DELTA_CKPT="logs/chris_pratt_gog_background1_2/delta.bin"
+# FROM_FILE="prompts/gog_chris_pratt_background3.txt"
+# KEYWORD="base_setting"
 
 
-accelerate launch src/diffusers_training.py \
-          --pretrained_model_name_or_path $MODEL_NAME  \
-          --output_dir ${OUTPUT_DIR}  \
-          --concepts_list ${CONCEPT_LIST} \
-          --with_prior_preservation --real_prior --prior_loss_weight=1.0 \
-          --resolution=512  \
-          --train_batch_size=2  \
-          --learning_rate=1e-5  \
-          --lr_warmup_steps=0 \
-          --max_train_steps=1000 \
-          --num_class_images=200 \
-          --scale_lr --hflip  \
-          --modifier_token "<new1>+<new2>" 
+# accelerate launch src/diffusers_training.py \
+#           --pretrained_model_name_or_path $MODEL_NAME  \
+#           --output_dir ${OUTPUT_DIR}  \
+#           --concepts_list ${CONCEPT_LIST} \
+#           --with_prior_preservation --real_prior --prior_loss_weight=1.0 \
+#           --resolution=512  \
+#           --train_batch_size=2  \
+#           --learning_rate=1e-5  \
+#           --lr_warmup_steps=0 \
+#           --max_train_steps=1000 \
+#           --num_class_images=200 \
+#           --scale_lr --hflip  \
+#           --modifier_token "<new1>+<new2>" 
 
-## sample 
-python src/diffusers_sample.py \
-    --delta_ckpt ${DELTA_CKPT} \
-    --ckpt ${MODEL_NAME} \
-    --from-file ${FROM_FILE} \
-    --keyword ${KEYWORD} \
+# ## sample 
+# python src/diffusers_sample.py \
+#     --delta_ckpt ${DELTA_CKPT} \
+#     --ckpt ${MODEL_NAME} \
+#     --from-file ${FROM_FILE} \
+#     --keyword ${KEYWORD} \
 
 # MODEL_NAME="CompVis/stable-diffusion-v1-4"
 # OUTPUT_DIR="./logs/jjanggu_and_scene8_2"
