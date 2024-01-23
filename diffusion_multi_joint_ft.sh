4b9c54665b07763f5d63d73ed7dae8aa0d88a4ab
taehyeon_eum <paul994782@gamil.com>
2024-01-23 23:12:27 +0000

    update ex14, 15

--- diffusion_multi_joint_ft.sh
+++ diffusion_multi_joint_ft.sh
@@ -1,8 +1,37 @@
+# # MODEL_NAME="CompVis/stable-diffusion-v1-4"
+# # OUTPUT_DIR="./logs/chris_pratt_gog_background1_2"
+# # CONCEPT_LIST="./assets/concept_list_gog.json"
+# # DELTA_CKPT="logs/chris_pratt_gog_background1_2/delta.bin"
+# # FROM_FILE="prompts/gog_chris_pratt_background3.txt"
+# # KEYWORD="base_setting"
+
+
+# # # accelerate launch src/diffusers_training.py \
+# # #           --pretrained_model_name_or_path $MODEL_NAME  \
+# # #           --output_dir ${OUTPUT_DIR}  \
+# # #           --concepts_list ${CONCEPT_LIST} \
+# # #           --with_prior_preservation --real_prior --prior_loss_weight=1.0 \
+# # #           --resolution=512  \
+# # #           --train_batch_size=2  \
+# # #           --learning_rate=1e-5  \
+# # #           --lr_warmup_steps=0 \
+# # #           --max_train_steps=1000 \
+# # #           --num_class_images=200 \
+# # #           --scale_lr --hflip  \
+# # #           --modifier_token "<new1>+<new2>" 
+
+# # ## sample 
+# # python src/diffusers_sample.py \
+# #     --delta_ckpt ${DELTA_CKPT} \
+# #     --ckpt ${MODEL_NAME} \
+# #     --from-file ${FROM_FILE} \
+# #     --keyword ${KEYWORD} \
+
 # MODEL_NAME="CompVis/stable-diffusion-v1-4"
-# OUTPUT_DIR="./logs/chris_pratt_gog_background1_2"
-# CONCEPT_LIST="./assets/concept_list_gog.json"
-# DELTA_CKPT="logs/chris_pratt_gog_background1_2/delta.bin"
-# FROM_FILE="prompts/gog_chris_pratt_background3.txt"
+# OUTPUT_DIR="./logs/jjanggu_and_scene8_2"
+# CONCEPT_LIST="./assets/concept_list_jjanggu_scene.json"
+# DELTA_CKPT="logs/jjanggu_and_scene8_2/delta.bin"
+# FROM_FILE="prompts/jjanggu.txt"
 # KEYWORD="base_setting"
 
 
@@ -27,27 +56,150 @@
 #     --from-file ${FROM_FILE} \
 #     --keyword ${KEYWORD} \
 
+
+# ## launch training script 
+# ## (2 GPUs recommended, increase --max_train_steps to 1000 if 1 GPU)
+# MODEL_NAME="CompVis/stable-diffusion-v1-4"
+# OUTPUT_DIR="./logs/jjanggu_and_scene9"
+# CONCEPT_LIST="./assets/concept_list_jjanggu_scene.json"
+# DELTA_CKPT="logs/jjanggu_and_scene9/delta.bin"
+# FROM_FILE="prompts/jjanggu.txt"
+# KEYWORD="base_setting"
+
+
+# # accelerate launch src/diffusers_training.py \
+# #           --pretrained_model_name_or_path $MODEL_NAME  \
+# #           --output_dir ${OUTPUT_DIR}  \
+# #           --concepts_list ${CONCEPT_LIST} \
+# #           --with_prior_preservation --real_prior --prior_loss_weight=1.0 \
+# #           --resolution=512  \
+# #           --train_batch_size=2  \
+# #           --learning_rate=1e-5  \
+# #           --lr_warmup_steps=0 \
+# #           --max_train_steps=2000 \
+# #           --num_class_images=200 \
+# #           --scale_lr --hflip  \
+# #           --modifier_token "<new1>+<new2>" 
+
+# ## sample 
+# python src/diffusers_sample.py \
+#     --delta_ckpt ${DELTA_CKPT} \
+#     --ckpt ${MODEL_NAME} \
+#     --from-file ${FROM_FILE} \
+#     --keyword ${KEYWORD} \
+
+
+# MODEL_NAME="CompVis/stable-diffusion-v1-4"
+# OUTPUT_DIR="./logs/jjanggu_and_scene10"
+# CONCEPT_LIST="./assets/concept_list_jjanggu_scene.json"
+# DELTA_CKPT="logs/jjanggu_and_scene10/delta.bin"
+# FROM_FILE="prompts/jjanggu.txt"
+# KEYWORD="base_setting"
+
+
+# # accelerate launch src/diffusers_training.py \
+# #           --pretrained_model_name_or_path $MODEL_NAME  \
+# #           --output_dir ${OUTPUT_DIR}  \
+# #           --concepts_list ${CONCEPT_LIST} \
+# #           --with_prior_preservation --real_prior --prior_loss_weight=1.0 \
+# #           --resolution=512  \
+# #           --train_batch_size=2  \
+# #           --learning_rate=1e-5  \
+# #           --lr_warmup_steps=0 \
+# #           --max_train_steps=1500 \
+# #           --num_class_images=200 \
+# #           --scale_lr --hflip  \
+# #           --modifier_token "<new1>+<new2>" 
+
+# ## sample 
+# python src/diffusers_sample.py \
+#     --delta_ckpt ${DELTA_CKPT} \
+#     --ckpt ${MODEL_NAME} \
+#     --from-file ${FROM_FILE} \
+#     --keyword ${KEYWORD} \
+
+
+# MODEL_NAME="CompVis/stable-diffusion-v1-4"
+# OUTPUT_DIR="./logs/jjanggu_and_scene11"
+# CONCEPT_LIST="./assets/concept_list_jjanggu_scene.json"
+# DELTA_CKPT="logs/jjanggu_and_scene11/delta.bin"
+# FROM_FILE="prompts/jjanggu.txt"
+# KEYWORD="base_setting"
+
+
+# # accelerate launch src/diffusers_training.py \
+# #           --pretrained_model_name_or_path $MODEL_NAME  \
+# #           --output_dir ${OUTPUT_DIR}  \
+# #           --concepts_list ${CONCEPT_LIST} \
+# #           --with_prior_preservation --real_prior --prior_loss_weight=1.0 \
+# #           --resolution=512  \
+# #           --train_batch_size=2  \
+# #           --learning_rate=1e-5  \
+# #           --lr_warmup_steps=0 \
+# #           --max_train_steps=1250 \
+# #           --num_class_images=200 \
+# #           --scale_lr --hflip  \
+# #           --modifier_token "<new1>+<new2>" 
+
+# ## sample 
+# python src/diffusers_sample.py \
+#     --delta_ckpt ${DELTA_CKPT} \
+#     --ckpt ${MODEL_NAME} \
+#     --from-file ${FROM_FILE} \
+#     --keyword ${KEYWORD} \
+
+
+
+# MODEL_NAME="CompVis/stable-diffusion-v1-4"
+# OUTPUT_DIR="./logs/jjanggu_and_scene12"
+# CONCEPT_LIST="./assets/concept_list_jjanggu_scene2.json"
+# DELTA_CKPT="logs/jjanggu_and_scene12/delta.bin"
+# FROM_FILE="prompts/jjanggu.txt"
+# KEYWORD="base_setting"
+
+
+# # accelerate launch src/diffusers_training.py \
+# #           --pretrained_model_name_or_path $MODEL_NAME  \
+# #           --output_dir ${OUTPUT_DIR}  \
+# #           --concepts_list ${CONCEPT_LIST} \
+# #           --with_prior_preservation --real_prior --prior_loss_weight=1.0 \
+# #           --resolution=512  \
+# #           --train_batch_size=2  \
+# #           --learning_rate=1e-5  \
+# #           --lr_warmup_steps=0 \
+# #           --max_train_steps=1500 \
+# #           --num_class_images=200 \
+# #           --scale_lr --hflip  \
+# #           --modifier_token "<new1>+<new2>" 
+
+# ## sample 
+# python src/diffusers_sample.py \
+#     --delta_ckpt ${DELTA_CKPT} \
+#     --ckpt ${MODEL_NAME} \
+#     --from-file ${FROM_FILE} \
+#     --keyword ${KEYWORD} \
+
 MODEL_NAME="CompVis/stable-diffusion-v1-4"
-OUTPUT_DIR="./logs/jjanggu_and_scene8_2"
-CONCEPT_LIST="./assets/concept_list_jjanggu_scene.json"
-DELTA_CKPT="logs/jjanggu_and_scene8_2/delta.bin"
+OUTPUT_DIR="./logs/jjanggu_and_scene13"
+CONCEPT_LIST="./assets/concept_list_jjanggu_scene13.json"
+DELTA_CKPT="logs/jjanggu_and_scene13/delta.bin"
 FROM_FILE="prompts/jjanggu.txt"
 KEYWORD="base_setting"
 
 
-# accelerate launch src/diffusers_training.py \
-#           --pretrained_model_name_or_path $MODEL_NAME  \
-#           --output_dir ${OUTPUT_DIR}  \
-#           --concepts_list ${CONCEPT_LIST} \
-#           --with_prior_preservation --real_prior --prior_loss_weight=1.0 \
-#           --resolution=512  \
-#           --train_batch_size=2  \
-#           --learning_rate=1e-5  \
-#           --lr_warmup_steps=0 \
-#           --max_train_steps=1000 \
-#           --num_class_images=200 \
-#           --scale_lr --hflip  \
-#           --modifier_token "<new1>+<new2>" 
+accelerate launch src/diffusers_training.py \
+          --pretrained_model_name_or_path $MODEL_NAME  \
+          --output_dir ${OUTPUT_DIR}  \
+          --concepts_list ${CONCEPT_LIST} \
+          --with_prior_preservation --real_prior --prior_loss_weight=1.0 \
+          --resolution=512  \
+          --train_batch_size=2  \
+          --learning_rate=1e-5  \
+          --lr_warmup_steps=0 \
+          --max_train_steps=1500 \
+          --num_class_images=200 \
+          --scale_lr --hflip  \
+          --modifier_token "<new1>+<new2>" 
 
 ## sample 
 python src/diffusers_sample.py \
@@ -56,45 +208,12 @@ python src/diffusers_sample.py \
     --from-file ${FROM_FILE} \
     --keyword ${KEYWORD} \
 
-
-## launch training script 
-## (2 GPUs recommended, increase --max_train_steps to 1000 if 1 GPU)
-MODEL_NAME="CompVis/stable-diffusion-v1-4"
-OUTPUT_DIR="./logs/jjanggu_and_scene9"
-CONCEPT_LIST="./assets/concept_list_jjanggu_scene.json"
-DELTA_CKPT="logs/jjanggu_and_scene9/delta.bin"
-FROM_FILE="prompts/jjanggu.txt"
-KEYWORD="base_setting"
-
-
-# accelerate launch src/diffusers_training.py \
-#           --pretrained_model_name_or_path $MODEL_NAME  \
-#           --output_dir ${OUTPUT_DIR}  \
-#           --concepts_list ${CONCEPT_LIST} \
-#           --with_prior_preservation --real_prior --prior_loss_weight=1.0 \
-#           --resolution=512  \
-#           --train_batch_size=2  \
-#           --learning_rate=1e-5  \
-#           --lr_warmup_steps=0 \
-#           --max_train_steps=2000 \
-#           --num_class_images=200 \
-#           --scale_lr --hflip  \
-#           --modifier_token "<new1>+<new2>" 
-
-## sample 
-python src/diffusers_sample.py \
-    --delta_ckpt ${DELTA_CKPT} \
-    --ckpt ${MODEL_NAME} \
-    --from-file ${FROM_FILE} \
-    --keyword ${KEYWORD} \
-
-
-MODEL_NAME="CompVis/stable-diffusion-v1-4"
-OUTPUT_DIR="./logs/jjanggu_and_scene10"
-CONCEPT_LIST="./assets/concept_list_jjanggu_scene.json"
-DELTA_CKPT="logs/jjanggu_and_scene10/delta.bin"
-FROM_FILE="prompts/jjanggu.txt"
-KEYWORD="base_setting"
+# MODEL_NAME="CompVis/stable-diffusion-v1-4"
+# OUTPUT_DIR="./logs/jjanggu_and_scene14"
+# CONCEPT_LIST="./assets/concept_list_jjanggu_scene14.json"
+# DELTA_CKPT="logs/jjanggu_and_scene14/delta.bin"
+# FROM_FILE="prompts/jjanggu.txt"
+# KEYWORD="base_setting"
 
 
 # accelerate launch src/diffusers_training.py \
@@ -111,51 +230,20 @@ KEYWORD="base_setting"
 #           --scale_lr --hflip  \
 #           --modifier_token "<new1>+<new2>" 
 
-## sample 
-python src/diffusers_sample.py \
-    --delta_ckpt ${DELTA_CKPT} \
-    --ckpt ${MODEL_NAME} \
-    --from-file ${FROM_FILE} \
-    --keyword ${KEYWORD} \
+# ## sample 
+# python src/diffusers_sample.py \
+#     --delta_ckpt ${DELTA_CKPT} \
+#     --ckpt ${MODEL_NAME} \
+#     --from-file ${FROM_FILE} \
+#     --keyword ${KEYWORD} \
 
 
-MODEL_NAME="CompVis/stable-diffusion-v1-4"
-OUTPUT_DIR="./logs/jjanggu_and_scene11"
-CONCEPT_LIST="./assets/concept_list_jjanggu_scene.json"
-DELTA_CKPT="logs/jjanggu_and_scene11/delta.bin"
-FROM_FILE="prompts/jjanggu.txt"
-KEYWORD="base_setting"
-
-
-# accelerate launch src/diffusers_training.py \
-#           --pretrained_model_name_or_path $MODEL_NAME  \
-#           --output_dir ${OUTPUT_DIR}  \
-#           --concepts_list ${CONCEPT_LIST} \
-#           --with_prior_preservation --real_prior --prior_loss_weight=1.0 \
-#           --resolution=512  \
-#           --train_batch_size=2  \
-#           --learning_rate=1e-5  \
-#           --lr_warmup_steps=0 \
-#           --max_train_steps=1250 \
-#           --num_class_images=200 \
-#           --scale_lr --hflip  \
-#           --modifier_token "<new1>+<new2>" 
-
-## sample 
-python src/diffusers_sample.py \
-    --delta_ckpt ${DELTA_CKPT} \
-    --ckpt ${MODEL_NAME} \
-    --from-file ${FROM_FILE} \
-    --keyword ${KEYWORD} \
-
-
-
-MODEL_NAME="CompVis/stable-diffusion-v1-4"
-OUTPUT_DIR="./logs/jjanggu_and_scene12"
-CONCEPT_LIST="./assets/concept_list_jjanggu_scene2.json"
-DELTA_CKPT="logs/jjanggu_and_scene12/delta.bin"
-FROM_FILE="prompts/jjanggu.txt"
-KEYWORD="base_setting"
+# MODEL_NAME="CompVis/stable-diffusion-v1-4"
+# OUTPUT_DIR="./logs/jjanggu_and_scene15"
+# CONCEPT_LIST="./assets/concept_list_jjanggu_scene15.json"
+# DELTA_CKPT="logs/jjanggu_and_scene15/delta.bin"
+# FROM_FILE="prompts/jjanggu.txt"
+# KEYWORD="base_setting"
 
 
 # accelerate launch src/diffusers_training.py \
@@ -172,9 +260,9 @@ KEYWORD="base_setting"
 #           --scale_lr --hflip  \
 #           --modifier_token "<new1>+<new2>" 
 
-## sample 
-python src/diffusers_sample.py \
-    --delta_ckpt ${DELTA_CKPT} \
-    --ckpt ${MODEL_NAME} \
-    --from-file ${FROM_FILE} \
-    --keyword ${KEYWORD} \
+# ## sample 
+# python src/diffusers_sample.py \
+#     --delta_ckpt ${DELTA_CKPT} \
+#     --ckpt ${MODEL_NAME} \
+#     --from-file ${FROM_FILE} \
+#     --keyword ${KEYWORD} \
